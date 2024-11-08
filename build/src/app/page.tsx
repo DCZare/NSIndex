b050f27f9b246d364f760914be174ead35763f38
DCZare <88459946+DCZare@users.noreply.github.com>
2024-11-08 15:48:52 -0500

    rush

--- build/src/app/page.tsx
+++ build/src/app/page.tsx
@@ -1,6 +1,12 @@
 'use client';
 
+import { useEffect } from 'react';
+
 export default function Home() {
+  useEffect(() => {
+    console.log("Home page rendered!");
+  }, []);
+
   return (
     <main>
       <h1>Hello, world!</h1>
