e23b265af688fdd1c2817114c1f4b191c42ec883
DCZare <88459946+DCZare@users.noreply.github.com>
2024-11-08 14:37:39 -0500

    working

--- build/src/app/page.tsx
+++ build/src/app/page.tsx
@@ -2,8 +2,8 @@
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
-      <h1 className="text-5xl">Hello, world!</h1>
+    <main>
+      <h1>Hello, world!</h1>
     </main>
   );
 }
